36d07f23f8ef24eb5ecb1de58db4e0d5f8e32cdf
fan yang <koopa@qq.com>
2013-09-10 16:17:46 -0400

    Chain of responsibility finished.

---
+++ src/designpattern/behavioral/chainofresponsibility/ConcreteHandler2.java
@@ -0,0 +1,23 @@
+package designpattern.behavioral.chainofresponsibility;
+
+public class ConcreteHandler2 extends Handler {
+
+
+
+	@Override
+	public void handleRequest(int request) {
+
+		if (request >= 10) {
+			
+			System.out.println("ConcreteHandler2");
+			
+		} else {
+			if (successor != null) {
+				successor.handleRequest(request);
+			}
+		}
+
+		
+	}
+
+}
