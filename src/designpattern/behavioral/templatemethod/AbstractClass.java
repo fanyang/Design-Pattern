14d6c46ee59634b8b6069e656ca29663edb66809
fan yang <koopa@qq.com>
2013-09-11 01:55:01 -0400

    Template method pattern done.

---
+++ src/designpattern/behavioral/templatemethod/AbstractClass.java
@@ -0,0 +1,18 @@
+package designpattern.behavioral.templatemethod;
+
+public abstract class AbstractClass {
+	
+	
+	public void templateMethod() {
+		
+		primitiveOperation1();
+		primitiveOperation2();
+		
+	}
+	
+	
+	public abstract void primitiveOperation1();
+	public abstract void primitiveOperation2();
+	
+
+}
