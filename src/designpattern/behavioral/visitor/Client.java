841336ff3bd5fd6270fa7c87021ad37e5fb08e71
fan yang <koopa@qq.com>
2013-09-11 11:33:42 -0400

    Refactor visitor

--- src/designpattern/behavioral/visitor/Client.java
+++ src/designpattern/behavioral/visitor/Client.java
@@ -5,11 +5,8 @@ public class Client {
 	public static void main(String[] args) {
 
 		ObjectStructure objectStructure= new ObjectStructure();
-		ConcreteElementA concreteElementA = new ConcreteElementA();
-		ConcreteElementB concreteElementB = new ConcreteElementB();
-		
-		objectStructure.add(concreteElementA);
-		objectStructure.add(concreteElementB);
+		objectStructure.add(new ConcreteElementA());
+		objectStructure.add(new ConcreteElementB());
 		
 		Visitor visitor= new ConcreteVisitor();
 		visitor.visitObjectStructure(objectStructure);
